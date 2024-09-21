4f3d439e329a7931b0a06be069c88f702eb3e258
Bruce <brucebenjaminpeters@gmail.com>
2024-09-21 16:24:42 -0700

    adding some stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import "./Home.css";
+import React, { useEffect } from 'react';
+// import "./Home.css";
 
 const bio = {
   title: "About Me",
@@ -12,23 +12,39 @@ const bio = {
 }
 
  export const Home = () => {
+
   return (
     <div className="home">
-      <div className="basic-info">
+      <div className="greeting-wrapper">
         <div className="greeting">
           <div>Hi,</div>
           <div>I'm Bruce</div>
         </div>
-        <div className="bio">
-          <div className="bio-title fade-in">About Me</div>
-          <div className="bio-content">
-            {bio.content.map((line, index) => (
-              <div className="fade-in" key={index}>{line}</div>
-            ))}
-          </div>
+      </div>
+      <div className="bio">
+        <div className="bio-title fade-in">About Me</div>
+        <div className="bio-content">
+          {bio.content.map((line, index) => (
+            <div className="fade-in" key={index}>
+              {line}
+            </div>
+          ))}
         </div>
       </div>
-      
+      <div className="profile-pic">
+        <img
+          src="https://avatars.githubusercontent.com/u/73262340?v=4"
+          alt="Bruce Peters"
+        />
+      </div>
+
+      <div className="contact-info">
+        <div className="contact-title fade-in">Contact Me</div>
+        <div className="contact-content">
+          <div className="fade-in">Email:</div>
+          <div className="fade-in">brucebpeters@gmail.com</div>
+        </div>
+      </div>
     </div>
   );
 }
